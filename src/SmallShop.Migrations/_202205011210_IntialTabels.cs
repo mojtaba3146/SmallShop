da19e06a463ee9c2400603b866da87f2fbfe31bd
mojtaba <mojtaba_kh@hotmail.com>
2022-05-02 22:20:48 +0430

    Create FluentMigration For SmallShop

---
+++ src/SmallShop.Migrations/_202205011210_IntialTabels.cs
@@ -0,0 +1,78 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallShop.Migrations
+{
+    [Migration(202205011210)]
+    public class _202205011210_IntialTabels : Migration
+    {
+        public override void Up()
+        {
+            CreateCategory();
+            CreateGoods();
+            CreatePurshaseInvoice();
+            CreateSaleInvoice();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Categories");
+            Delete.Table("Goodss");
+            Delete.Table("PurchaseInvoices");
+            Delete.Table("SaleInvoices");
+        }
+
+        private void CreateSaleInvoice()
+        {
+            Create.Table("SaleInvoices")
+                            .WithColumn("InvoiceNum").AsInt32().PrimaryKey().NotNullable()
+                            .WithColumn("Date").AsDateTime().NotNullable()
+                            .WithColumn("Count").AsInt32().NotNullable()
+                            .WithColumn("Price").AsInt32().NotNullable()
+                            .WithColumn("BuyerName").AsString(50).NotNullable()
+                            .WithColumn("GoodsId").AsInt32().NotNullable()
+                            .ForeignKey("FK_SaleInvoices_Goods", "Goodss", "GoodsCode")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        private void CreatePurshaseInvoice()
+        {
+            Create.Table("PurchaseInvoices")
+                            .WithColumn("InvoiceNum").AsInt32().PrimaryKey().NotNullable()
+                            .WithColumn("Date").AsDateTime().NotNullable()
+                            .WithColumn("Count").AsInt32().NotNullable()
+                            .WithColumn("Price").AsInt32().NotNullable()
+                            .WithColumn("SellerName").AsString(50).NotNullable()
+                            .WithColumn("GoodsId").AsInt32().NotNullable()
+                            .ForeignKey("FK_PurchaseInvoices_Goods", "Goodss", "GoodsCode")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        private void CreateGoods()
+        {
+            Create.Table("Goodss")
+                            .WithColumn("GoodsCode").AsInt32().PrimaryKey().NotNullable()
+                            .WithColumn("Name").AsString(50).NotNullable()
+                            .WithColumn("Price").AsInt32().NotNullable()
+                            .WithColumn("GoodsInventory").AsInt32().Nullable()
+                            .WithColumn("MinInventory").AsInt32().NotNullable()
+                            .WithColumn("MaxInventory").AsInt32().NotNullable()
+                            .WithColumn("SellCount").AsInt32().Nullable()
+                            .WithColumn("CategoryId").AsInt32().NotNullable()
+                            .ForeignKey("FK_Goodss_Categories", "Categories", "Id")
+                            .OnDelete(System.Data.Rule.None);
+        }
+
+        private void CreateCategory()
+        {
+            Create.Table("Categories")
+                              .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                              .WithColumn("Title").AsString(50).NotNullable();
+        }
+
+    }
+}
