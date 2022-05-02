9193ff79ada0678a52d06d8a718ab1d1e31471d5
mojtaba <mojtaba_kh@hotmail.com>
2022-05-02 22:33:53 +0430

    Add nessesary files for spec and unit tests and install packages

---
+++ src/SmallShop.Specs/Infrastructure/EFDataContextDatabaseFixture.cs
@@ -0,0 +1,21 @@
+﻿using SmallShop.Persistence.EF;
+using Xunit;
+
+namespace SmallShop.Specs.Infrastructure
+{
+    [Collection(nameof(ConfigurationFixture))]
+    public class EFDataContextDatabaseFixture : DatabaseFixture
+    {
+        readonly ConfigurationFixture _configuration;
+
+        public EFDataContextDatabaseFixture(ConfigurationFixture configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public EFDataContext CreateDataContext()
+        {
+            return new EFDataContext(_configuration.Value.DbConnectionString);
+        }
+    }
+}
