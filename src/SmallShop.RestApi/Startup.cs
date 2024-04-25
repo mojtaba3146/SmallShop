89b81186741a4e70c70dad04bed524dc185a420d
m.khoshnam <mojtaba_kh@hotmail.com>
2024-04-25 12:27:29 +0330

    refactor

--- src/SmallShop.RestApi/Startup.cs
+++ src/SmallShop.RestApi/Startup.cs
@@ -1,21 +1,14 @@
 using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SmallShop.Infrastructure.Application;
 using SmallShop.Persistence.EF;
 using SmallShop.Persistence.EF.Categories;
 using SmallShop.Services.Categories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SmallShop.RestApi
 {
